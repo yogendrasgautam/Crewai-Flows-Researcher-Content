ebf8bdbf6152e09567edf00772f962ca33ba89c3
Sourangshu Pal <paulbindass@gmail.com>
2025-04-06 17:07:50 +0530

    Updates

--- src/researcher_content/main.py
+++ src/researcher_content/main.py
@@ -4,7 +4,7 @@ from typing import List, Dict
 from pydantic import BaseModel, Field
 from crewai import LLM
 from crewai.flow.flow import Flow, listen, start
-from reasearcher_content.crews.content_crew.content_crew import ContentCrew
+from researcher_content.crews.content_crew.content_crew import ContentCrew
 
 # Define our models for structured data
 class Section(BaseModel):
